aad1c465d0bb0db0dd50cb06912e7fb09f48bd08
glasset <glasset@github.com>
2016-09-24 20:45:22 +0200

    handle segment_64

--- nm_core.c
+++ nm_core.c
@@ -6,12 +6,12 @@
 /*   By: glasset <glasset@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/05 16:42:01 by glasset           #+#    #+#             */
-/*   Updated: 2016/09/18 17:49:06 by glasset          ###   ########.fr       */
+/*   Updated: 2016/09/24 20:44:35 by glasset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "nm.h"
 
-char	type(int type, int sec, int value)
+char	type(int type, int sec, int value, t_flags *flags)
 {
 	char	c;
 	c = '?';
@@ -28,13 +28,13 @@ char	type(int type, int sec, int value)
 			c = 'A';
 			break;
 		case N_SECT:
-	//		if (sec == flags->text_nsect) TODO plug struct
-	//			c = 'T';
-	//		else if (sec == flags->data_nsect)
-	//			c = 'D';
-	//		else if (sec == flags->bss_nsect)
-	//			c = 'B';
-	//		else
+			if (sec == flags->text_nsect)
+				c = 'T';
+			else if (sec == flags->data_nsect)
+				c = 'D';
+			else if (sec == flags->bss_nsect)
+				c = 'B';
+			else
 				c = 'S';
 			break;
 		case N_INDR:
@@ -47,7 +47,25 @@ char	type(int type, int sec, int value)
 	return c;
 }
 
-void	print(int nsyms, int symoff, int stroff, char *ptr)
+// TODO migrate to libft
+int				ft_hexa(unsigned int n)
+{
+	unsigned int			a;
+	unsigned int			u;
+	char					*str;
+
+	str = "0123456789abcdef";
+	u = 0;
+	a = n % 16;
+	n = n / 16;
+	if (n > 0)
+		u = u + ft_hexa(n);
+	write(1, &str[a], 1);
+	u++;
+	return (u);
+}
+
+void	print(int nsyms, int symoff, int stroff, char *ptr, t_flags *flags)
 {
 	int i;
 	struct nlist_64		*list;
@@ -60,22 +78,61 @@ void	print(int nsyms, int symoff, int stroff, char *ptr)
 
 	while (i < nsyms)
 	{
-		ft_putnbr(list[i].n_value);
-		ft_putstr("\t");
-		ft_putchar(type(list[i].n_type, list[i].n_sect, list[i].n_value));
-		ft_putstr("\t");
+		// TODO clean print  + padding
+		if (list[i].n_value) {
+		ft_putstr("000000000000");
+		ft_hexa(list[i].n_value);
+		}
+		else {
+		ft_putstr("                ");
+		}
+		ft_putstr(" ");
+		ft_putchar(type(list[i].n_type, list[i].n_sect, list[i].n_value, flags));
+		ft_putstr(" ");
 		ft_putendl(strtab + list[i++].n_un.n_strx);
+	}
+}
+
+void	handle_segment(t_flags *flags, struct segment_command_64 *seg)
+{
+	int					i;
+	struct section_64	*sec;
+
+	i = 0;
+	sec = (struct section_64 *) ((char *)seg + sizeof(struct segment_command_64));
+	while (i < seg->nsects)
+	{
+		if (ft_strcmp((sec + i)->sectname, SECT_TEXT) == 0 &&
+				ft_strcmp((sec + i)->segname, SEG_TEXT) == 0)
+		{
+			flags->text_nsect = i + 1;
+		}
+		else if (ft_strcmp((sec + i)->sectname, SECT_DATA) == 0 &&
+				ft_strcmp((sec + i)->segname, SEG_DATA) == 0)
+		{
+			flags->data_nsect = i + 1;
+		}
+		else if (ft_strcmp((sec + i)->sectname, SECT_BSS) == 0 &&
+				ft_strcmp((sec + i)->segname, SEG_DATA) == 0)
+		{
+			flags->bss_nsect = i + 1;
+		}
+		i++;
 	}
 }
 
 void	header_64(char *ptr)
 {
+	t_flags					flags;
 	struct mach_header_64	*header;
 	struct load_command		*command;
 	struct symtab_command	*sym;
 	unsigned long int		i;
 	int						ncmds;
 
+	flags.text_nsect = NO_SECT;
+	flags.data_nsect = NO_SECT;
+	flags.bss_nsect = NO_SECT;
 	i = 0;
 	header = (struct mach_header_64 *)ptr;
 	command = (void *)ptr + sizeof(*header);
@@ -84,9 +141,12 @@ void	header_64(char *ptr)
 		if (command->cmd == LC_SYMTAB)
 		{
 			sym = (struct symtab_command *)command;
-			print(sym->nsyms, sym->symoff, sym->stroff, ptr);
-			 // TODO  clean / print
+			print(sym->nsyms, sym->symoff, sym->stroff, ptr, &flags);
 			break;
+		}
+		if (command->cmd == LC_SEGMENT_64)
+		{
+			handle_segment(&flags, (struct segment_command_64 *)command);
 		}
 		command = (void *)command + command->cmdsize;
 		i++;
